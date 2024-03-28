0dc108b35ee1bec856b5f4cb941d5db995b500af
zerzkhane <zerzkhanemourad@gmail.com>
2024-03-28 01:24:23 +0000

    add header section

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,17 @@
 export const NAV_LINKS = [
-  
-    { href:'' ,label:'ABOUT' },
-    { href:'' ,label:'Membership' },
-    { href:'' ,label:'News & Events' },
-    { href:'' ,label:'Contact us' },
-    
+
+    { href: '', label: 'ABOUT' },
+    { href: '', label: 'Membership' },
+    { href: '', label: 'News & Events' },
+    { href: '', label: 'Contact us' },
+
+]
+
+export const Statis = [
+
+    { number: 800,  label: 'Partners' },
+    { number: 55,   label:  'Countries' },
+    { number: '4 500', label: 'Opportunities' },
+    { number: '1 529', label: 'Companies' },
+
 ]
